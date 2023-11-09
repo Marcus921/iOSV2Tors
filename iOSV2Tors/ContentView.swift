b60f8361f1db8143b1eafd6eddc61e9c8f70f3d8
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2023-11-09 09:14:14 +0100

    Initial Commit

---
+++ iOSV2Tors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSV2Tors
+//
+//  Created by Marcus Malmgren on 2023-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSV2Tors/iOSV2TorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSV2TorsApp.swift
+//  iOSV2Tors
+//
+//  Created by Marcus Malmgren on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct iOSV2TorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
